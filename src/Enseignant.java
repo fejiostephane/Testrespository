054cf82dac33f5ffcf6fba64fbea8fc2fe2dc171
liam <stephanefoyet6@mail.com>
2022-12-12 17:17:48 +0100

    debut du code java poo

---
+++ src/Enseignant.java
@@ -0,0 +1,10 @@
+public class Enseignant {
+    String nom = "fejio";
+    String prenom = "foyet";
+    int age = 19 ;
+    String email = "foyetfejio@gmail.com";
+    String ville = "douala";
+    String matricule = "KIA 34-54";
+    String sexe = "masculin";
+    int numero = 698919043;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    // main method
     public static void main(String[] args) {
         System.out.println("Hello world!");
         FirstClass myObjet = new FirstClass();
@@ -10,5 +11,40 @@ public class Main {
 
         Etudiant Stephane = new Etudiant();
         System.out.println("je m'appele :"+Stephane.nom + Stephane.prenom+ ",j'ai " +Stephane.age+"ans" +",je reste à " +Stephane.ville+ ",mon mail est " + Stephane.email);
+
+        Enseignant Foyet = new Enseignant();
+        System.out.println("je m'appele :"+Foyet.nom + Foyet.prenom+ ",j'ai " +Foyet.age+"ans" +",je reste à " +Foyet.ville+ ",mon mail est " + Foyet.email);
+
+        myMethod();
+        myStaticMethod();
+       // myPublicMethod(); on peut pas l'appeler dans une static method
+
+        Main Brayane = new Main();
+        Brayane.myPublicMethod();
+
+        Main myCar = new Main();
+        myCar.fullThrottle();
+        myCar.speed(200);
+
+
     }
+    static void myMethod() {
+        System.out.println("hello word");
+
+    }
+    static void myStaticMethod() {
+        System.out.println("Static methods can be called without creating objects");
+    }
+    public void myPublicMethod() {
+        System.out.println("Public methods must be called by creating objects");
+    }
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can!");
+
+    }
+    public  void speed(int maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed);
+
+    }
+
 }
